ccfab69f0220380a3f83890d1585f5751556d492
Kasparov <ianjkiplagat@gmail.com>
2021-04-15 14:42:47 +0300

    test API fetch

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 # Views
 @app.route('/')
@@ -8,5 +9,7 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    # Getting all news
+    everything_news = get_news('everything')
     title = 'Home - The Daily Planet '
-    return render_template('index.html', title=title)
+    return render_template('index.html', title=title, everything = everything_news)
